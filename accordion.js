ee01907cbd27897c1dc7285ebcd866efa2d75c11
Gavri Kepets <gkepets@gmail.com>
2018-01-02 17:58:17 -0500

    Sync

--- accordion.js
+++ accordion.js
@@ -3,13 +3,29 @@ function generateAccordion(listP, type){
 	var list = listP;
 	var listOfStrings = [];
 
-	for (var i = 0, len = list.length; i < len; i++) {
+	var maximumDepth = 0;
+
+
+	for (let i = 0, len = list.length; i < len; i++) {
 		console.log(list[i].path);
 		listOfStrings.push(list[i].path);
+		
+		var counter = 0;
+		for(let x = 0; x < list[i].path.length; x++){
+			if(list[i].path.charAt(x) == '/'){
+				counter++;
+			}
+		}
+		if(counter >= maximumDepth){
+			maximumDepth = counter;
+		}
 	}
 	
 	if(type == "accordion"){
 		console.log("Starting Accordion");
+		for(let i = 0; i < maximumDepth; i++){
+			console.log("Max " + maximumDepth + "  " + i);
+		}
 	}
 	
 	
