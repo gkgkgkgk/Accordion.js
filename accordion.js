9e7717cb17011ae5e6c2f675c5c971c77b1077c8
Gavri Kepets <gkepets@gmail.com>
2018-01-03 10:01:14 -0500

    Its dead...

--- accordion.js
+++ accordion.js
@@ -5,7 +5,9 @@ function generateAccordion(listP, type){
 
 	var maximumDepth = 0;
 
-
+	var html = [];
+	
+	
 	for (let i = 0, len = list.length; i < len; i++) {
 		console.log(list[i].path);
 		listOfStrings.push(list[i].path);
@@ -24,7 +26,7 @@ function generateAccordion(listP, type){
 	if(type == "accordion"){
 		console.log("Starting Accordion");
 		for(let i = 0; i < maximumDepth; i++){
-			console.log("Depth: " + i);
+			//console.log("Depth: " + i);
 			for(let x = 0; x < listOfStrings.length; x++){
 				var counter = -1;
 				for(let y = 0; y < listOfStrings[x].length; y++){
@@ -33,12 +35,34 @@ function generateAccordion(listP, type){
 					}
 				}
 				if(counter == i){ // if it is on the same layer
-					console.log(listOfStrings[x] + " same layer " + i);
+					//console.log(listOfStrings[x] + " same layer " + i);
+					if(counter == 0){
+						html.push("<li>" + listOfStrings[x], "</li>");
+					}
+					else{
+						for(let z = 0; z < counter; z++){
+							var newHtml = [];
+							for(let c = 0; c < html.length; c++){
+								var newCounter = 0;
+								/*for(let y = 0; y < html[c].length; y++){
+									if(html[c].charAt(y) == '/'){
+										counter++;
+									}
+								}*/
+								if(listOfStrings[x].indexOf(html[c]) != 0/* && counter - 1 == newCounter*/){
+									newHtml.push("<ul>", "<li>" + listOfStrings[x], "</li>", "</ul>");
+								}
+								else{
+									newHtml.push(html[c]);
+								}
+							}
+						}
+					}
 				}
 			}
 		}
 	}
 	
 	
-	return "<p>Success</p>";
+	return "<ul>" + html.join("") + "</ul>";
 }
