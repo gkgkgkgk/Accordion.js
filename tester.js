1b65514d10e95e03dd5cff17c8374c6e0a1b33f2
gkgkgkgk <gkepets@gmail.com>
2018-01-04 12:14:59 -0500

    Adding Color Support

--- tester.js
+++ tester.js
@@ -1,30 +1,37 @@
 var one = {
     name: "one",
-    path: "/one"
+    path: "/one",
+	color: "f4dc41"
 }
 var two = {
     name: "two",
-    path: "/two"
+    path: "/two",
+	color: "f44242"
 }
 var three = {
     name: "three",
-    path: "/three"
+    path: "/three",
+	color: "41dcf4"
 }
 var oneOne = {
     name: "oneOne",
-    path: "/one/oneOne"
+    path: "/one/oneOne",
+	color: "417ff4"
 }
 var oneTwo = {
     name: "oneTwo",
-    path: "/one/oneTwo"
+    path: "/one/oneTwo",
+	color: "41f4ac"
 }
 var twoOne = {
     name: "twoOne",
-    path: "/two/twoOne"
+    path: "/two/twoOne",
+	color: "41f4ac"
 }
 var twoOneOne = {
     name: "twoOneOne",
-    path: "/two/twoOne/twoOneOne"
+    path: "/two/twoOne/twoOneOne",
+	color: "76f441"
 }
 
 var objects = [one, two, three, oneOne, oneTwo, twoOne, twoOneOne];
